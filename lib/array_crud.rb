301c33fd7259a65b8651edac3b01b7b56cd5ef71
Megan Adams <meganoliviawendyadams@gmail.com>
2019-06-25 09:28:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,8 +20,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow", "I", "am", "really", "learning"]
-    element = "arrays!"
+  array = ["I", "am", "really", "learning"]
+    element = "wow"
     array.pop element
 end
 
